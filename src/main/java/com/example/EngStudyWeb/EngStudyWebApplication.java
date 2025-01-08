66bf965cda59e39f95e21d601922793dfce3e607
yongminkim0501 <yongmingim166@gmail.com>
2025-01-08 14:48:57 +0900

    웹 크롤링을 통하여 웹 페이지 영단어 사전 (공부용) 구현을 위한 스프링 프로젝트

---
+++ src/main/java/com/example/EngStudyWeb/EngStudyWebApplication.java
@@ -0,0 +1,14 @@
+package com.example.EngStudyWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngStudyWebApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(EngStudyWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EngStudyWeb/EngStudyWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EngStudyWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngStudyWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
